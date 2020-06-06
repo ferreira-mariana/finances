584fe8feae35e9432ecc197c3a41f0d0deffcdc8
Mariana Ferreira <ferreira_mariana@id.uff.br>
2020-06-06 15:45:26 -0400

    month template

--- finances/views.py
+++ finances/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from .models import AccountEntry
+
+import datetime
 
 def index(request):
     latest_entry_list = AccountEntry.objects.order_by('-date')[:5]
@@ -18,6 +20,12 @@ def detail(request, entry_id):
     entry = get_object_or_404(AccountEntry, pk=entry_id)
     return render(request, 'finances/detail.html', {'entry': entry})
 
-def month(request, year, month):
-    d = '{}/{}'.format(year,month)
-    return HttpResponse("You're looking at month %s." % d)
+def month(request, year, month):    
+    entry_list = get_list_or_404(AccountEntry, date__year=year, date__month=month)
+    month_name = datetime.date(year, month, 1).strftime("%B")
+    context = {
+        'month_entries': entry_list,
+        'year': year,
+        'month': month_name,
+    }
+    return render(request, 'finances/month.html', context)
