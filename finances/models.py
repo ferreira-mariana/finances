d4cdc67fe9a54e5b76c1f470c53c5d85469a768d
Mariana Ferreira <ferreira_mariana@id.uff.br>
2020-06-06 01:05:32 -0400

    created model account entry

--- finances/models.py
+++ finances/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from enum import Enum
+
 import datetime
 
-class Expense(models.Model):
+class AccountEntry(models.Model):
+    EntryType = (
+        ('in', 'income'),
+        ('out', 'expense')
+    )
     name = models.CharField(max_length=100)
     amount = models.FloatField()
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default='other')
     date = models.DateField()
+    entry_type = models.CharField(choices=EntryType, max_length=10)
 
     def __str__(self):
         return self.name
@@ -13,11 +20,4 @@ class Expense(models.Model):
     def was_spent_recently(self):
         return self.date >= datetime.date.today() - datetime.timedelta(days=7)
 
-class Income(models.Model):
-    name = models.CharField(max_length=100)
-    amount = models.FloatField()
-    category = models.CharField(max_length=100)
-    date = models.DateField()
-
-    def __str__(self):
-        return self.name
+# django gerou como account entrys em vez de account entries 
