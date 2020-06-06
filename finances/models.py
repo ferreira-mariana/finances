21caf53cd0caada6f6af58eb0b69ae3e88d235bb
Mariana Ferreira <ferreira_mariana@id.uff.br>
2020-06-05 20:23:39 -0400

    creating models expense and income

--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.FloatField()
+    category = models.CharField(max_length=100)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    def was_spent_recently(self):
+        return self.date >= datetime.date.today() - datetime.timedelta(days=7)
+
+class Income(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.FloatField()
+    category = models.CharField(max_length=100)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
