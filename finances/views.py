09052a4ef4ca15788c6275acb1b8915aed404e44
Mariana Ferreira <ferreira_mariana@id.uff.br>
2020-06-06 19:38:42 -0400

    total month spent, incomes, savings

--- finances/views.py
+++ finances/views.py
@@ -7,8 +7,8 @@ import datetime
 
 def index(request):
     latest_entry_list = AccountEntry.objects.order_by('-date')[:5]
-    latest_expense_list = AccountEntry.objects.filter(entry_type='out')
-    latest_income_list = AccountEntry.objects.filter(entry_type='in')
+    latest_expense_list = AccountEntry.objects.filter(entry_type='out')[:5]
+    latest_income_list = AccountEntry.objects.filter(entry_type='in')[:5]
     context = {
         'latest_entry_list': latest_entry_list,
         'latest_expense_list': latest_expense_list,
@@ -21,10 +21,18 @@ def detail(request, entry_id):
     return render(request, 'finances/detail.html', {'entry': entry})
 
 def month(request, year, month):    
-    entry_list = get_list_or_404(AccountEntry, date__year=year, date__month=month)
+    expense_list = get_list_or_404(AccountEntry, date__year=year, date__month=month, entry_type='out')
+    income_list = get_list_or_404(AccountEntry, date__year=year, date__month=month, entry_type='in')
+    total_expense = sum(e.amount for e in expense_list)
+    total_income = sum(i.amount for i in income_list)
+    savings = total_income - total_expense
     month_name = datetime.date(year, month, 1).strftime("%B")
     context = {
-        'month_entries': entry_list,
+        'month_expenses': expense_list,
+        'month_incomes': income_list,
+        'total_expense': total_expense,
+        'total_income': total_income,
+        'savings': savings,
         'year': year,
         'month': month_name,
     }
