9d16492720690609a32adbe9e9b4f7c7a9b1b524
Mariana Ferreira <ferreira_mariana@id.uff.br>
2020-06-13 18:31:35 -0400

    changing url

--- finances/urls.py
+++ finances/urls.py
@@ -6,5 +6,5 @@ app_name = 'finances'
 urlpatterns = [
     path('month/<int:year>/<int:month>/', views.month, name='month'),
     path('new_entry/', views.new_entry, name='new_entry'),
-    path('/', views.index, name='index')
+    path('', views.index, name='index')
 ]
